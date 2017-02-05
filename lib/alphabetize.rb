e7e6efae42dad0c93901f6591d57a5504fda242a
Gee Li <geeyung.li@gmail.com>
2017-02-05 17:10:13 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
+  #.sort_by passes value and sorts into a new array
+    #.split converts entire string into an array.
+      #.map iterates and returns new collection with changes given in the block
+        #.index returns index value based on character of the alphabet.
 end
